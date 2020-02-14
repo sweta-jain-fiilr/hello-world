8330d9b85935f0508367689e589f86a9f851dd6f
sweta <51468556+sweta-jain-fillr@users.noreply.github.com>
2020-02-14 15:21:02 +1100

    Add files via upload

---
+++ shweta.sh
@@ -0,0 +1,27 @@
+
+Last login: Fri Feb 14 14:58:23 on ttys000
+Fillrs-MacBook-Pro:~ fillrlaptop$ vi scriptsample.sh
+Fillrs-MacBook-Pro:~ fillrlaptop$ #!/bin/sh
+Fillrs-MacBook-Pro:~ fillrlaptop$ ls
+A:Users:fillrlaptop:Applications:Chromium.apppplications
+Applications
+Desktop
+Documents
+Downloads
+Dropbox
+Library
+Movies
+Music
+Pictures
+Public
+practice
+scriptsample.sh
+temp
+unixstuff
+work
+Fillrs-MacBook-Pro:~ fillrlaptop$ bash scriptsample.sh
+Fillrs-MacBook-Pro:~ fillrlaptop$ #!/bin/sh
+Fillrs-MacBook-Pro:~ fillrlaptop$ #sample scripting
+Fillrs-MacBook-Pro:~ fillrlaptop$ pwd
+/Users/fillrlaptop
+Fillrs-MacBook-Pro:~ fillrlaptop$
